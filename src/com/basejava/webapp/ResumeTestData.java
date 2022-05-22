03285b2770be4f49d6a5685d45fdebb3835698ee
naliev <sms290@yandex.ru>
2022-05-22 11:06:15 +0300

    new test for new classes

--- src/com/basejava/webapp/ResumeTestData.java
+++ src/com/basejava/webapp/ResumeTestData.java
@@ -2,21 +2,27 @@ package com.basejava.webapp;
 
 import com.basejava.webapp.model.*;
 
+import java.util.Date;
 import java.util.HashMap;
 
 public class ResumeTestData {
     public static void main(String[] args) {
         Resume resume = new Resume("001", "Nikita Aliev");
         resume.putContact(ContactType.EMAIL, "dudadead@gmail.com");
-        resume.putContact(ContactType.GITHUB_PROFILE, "Naliev");
+        resume.putContact(ContactType.GITHUB, "Naliev");
         TextSection personal = new TextSection("Easy going, polite person");
         resume.putSection(SectionType.PERSONAL, personal);
         ListSection qualificationList = new ListSection();
         qualificationList.addText("Arrays", "Collections", "jUnit 4", "Exceptions", "Reflection", "Generics", "logging", "Template method", "Singleton method");
         resume.putSection(SectionType.QUALIFICATIONS, qualificationList);
-        DatedSection experienceUniversity = new DatedSection("Vologda Regional State Institute", "09/2017 - 07.2021", "Informatics and Computer Engineering");
-        DatedSection experienceExternal = new DatedSection("JavaOps", "05/2012 - 07/2022", "BaseJava", "Create a resume repository project");
-        resume.putSection(SectionType.EDUCATION, experienceUniversity, experienceExternal);
+        Organization vogu = new Organization("Vologda university","");
+        vogu.addPeriod(new OccupationPeriod("09/2017", "07.2021",
+                "Informatics and Computer Engineering",""));
+        resume.putSection(SectionType.EDUCATION, new OrganizationSection(vogu));
+        //OrganizationSection experienceUniversity = new OrganizationSection(vogu);
+        //experienceUniversity.addOrganization(new Organization());
+        //OrganizationSection experienceExternal = new OrganizationSection("JavaOps", "05/2012 - 07/2022", "BaseJava", "Create a resume repository project");
+        //resume.putSection(SectionType.EDUCATION, experienceUniversity, experienceExternal);
 
         printResume(resume);
     }
