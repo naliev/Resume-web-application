f722d5b59122faf008f5893d292b3b270c7b2a9b
naliev <sms290@yandex.ru>
2022-05-25 22:55:57 +0300

    Added new type of Storage

---
+++ src/com/basejava/webapp/storage/AbstractFileStorage.java
@@ -0,0 +1,89 @@
+package com.basejava.webapp.storage;
+
+import com.basejava.webapp.model.Resume;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Objects;
+
+public abstract class AbstractFileStorage extends AbstractStorage<File> {
+    private final File directory;
+
+    protected AbstractFileStorage(File directory) {
+        Objects.requireNonNull(directory);
+        if (!directory.isDirectory()) {
+            throw new IllegalArgumentException((directory.getAbsolutePath() + "is not a directory"));
+        }
+        if (!directory.canRead() || !directory.canWrite()) {
+            throw new IllegalArgumentException(directory.getAbsolutePath() + "is not readable/writeable");
+        }
+        this.directory = directory;
+    }
+
+    protected abstract void doWrite(Resume r, File file) throws IOException;
+
+    protected abstract Resume doRead(File file) throws IOException;
+
+    @Override
+    protected List<Resume> doGetAll() {
+        return null;
+    }
+
+    @Override
+    protected Resume doGet(File file) {
+        try {
+            return doRead(file);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    protected void doUpdate(Resume r, File file) {
+        try {
+            doWrite(r, file);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    protected void doSave(Resume r, File file) {
+        try {
+            file.createNewFile();
+            doWrite(r, file);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    protected void doDelete(File file) {
+        if (!file.delete()) {
+            throw new RuntimeException(file.getAbsolutePath() + "is cannot be deleted");
+        }
+    }
+
+    @Override
+    protected File getSearchKey(String uuid) {
+        return new File(directory, uuid);
+    }
+
+    @Override
+    protected boolean isExist(File file) {
+        return file.exists();
+    }
+
+    @Override
+    public int size() {
+        return directory.listFiles().length;
+    }
+
+    @Override
+    public void clear() {
+        for (File file : directory.listFiles()) {
+            doDelete(file);
+        }
+    }
+}
