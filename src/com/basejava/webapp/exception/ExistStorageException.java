929786797d3c87ab7b0b6f2b2470917a81b62efd
naliev <sms290@yandex.ru>
2022-05-28 23:32:59 +0300

    Exception minor fix

--- src/com/basejava/webapp/exception/ExistStorageException.java
+++ src/com/basejava/webapp/exception/ExistStorageException.java
@@ -2,6 +2,6 @@ package com.basejava.webapp.exception;
 
 public class ExistStorageException extends StorageException {
     public ExistStorageException(String uuid) {
-        super("Resume with " + uuid + " already exist", uuid);
+        super("Resume with uuid: " + uuid + " already exists", uuid);
     }
 }
