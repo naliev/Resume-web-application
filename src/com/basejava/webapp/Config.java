4563e34fc092d5281bd29f6038eb6c3a524bc897
naliev <sms290@yandex.ru>
2022-06-21 21:32:04 +0300

    Add method to initiate sql storage via Config

--- src/com/basejava/webapp/Config.java
+++ src/com/basejava/webapp/Config.java
@@ -1,5 +1,7 @@
 package com.basejava.webapp;
 
+import com.basejava.webapp.storage.SqlStorage;
+
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
@@ -11,17 +13,13 @@ public class Config {
     private static final Config INSTANCE = new Config();
     private final Properties props = new Properties();
     private final File storageDir;
-    private final String dbUrl;
-    private final String dbUser;
-    private final String dbPass;
+    private final SqlStorage sqlStorage;
 
     private Config() {
         try (InputStream is = Files.newInputStream(PROPS.toPath())) {
             props.load(is);
             storageDir = new File(props.getProperty("storage.dir"));
-            dbUrl = props.getProperty("db.url");
-            dbUser = props.getProperty("db.user");
-            dbPass = props.getProperty("db.password");
+            sqlStorage = new SqlStorage(props.getProperty("db.url"), props.getProperty("db.user"), props.getProperty("db.password"));
         } catch (IOException e) {
             throw new IllegalStateException("Invalid config file" + PROPS.getAbsolutePath());
         }
@@ -35,15 +33,7 @@ public class Config {
         return storageDir;
     }
 
-    public String getDbUrl() {
-        return dbUrl;
-    }
-
-    public String getDbUser() {
-        return dbUser;
-    }
-
-    public String getDbPass() {
-        return dbPass;
+    public SqlStorage getSqlStorage() {
+        return sqlStorage;
     }
 }
--- test/com/basejava/webapp/storage/SqlStorageTest.java
+++ test/com/basejava/webapp/storage/SqlStorageTest.java
@@ -4,11 +4,6 @@ import com.basejava.webapp.Config;
 
 public class SqlStorageTest extends AbstractStorageTest {
     public SqlStorageTest() {
-        super(new SqlStorage(
-                        Config.getConfig().getDbUrl(),
-                        Config.getConfig().getDbUser(),
-                        Config.getConfig().getDbPass()
-                )
-        );
+        super(Config.getConfig().getSqlStorage());
     }
 }
