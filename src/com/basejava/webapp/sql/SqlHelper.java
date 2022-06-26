adc6f5c5a273d231ac0302d997b6e81f8563fc5b
naliev <sms290@yandex.ru>
2022-06-26 20:52:22 +0300

    Init new interface for executing queries in a transaction

--- src/com/basejava/webapp/sql/SqlHelper.java
+++ src/com/basejava/webapp/sql/SqlHelper.java
@@ -1,5 +1,7 @@
 package com.basejava.webapp.sql;
 
+import com.basejava.webapp.exception.StorageException;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
@@ -11,12 +13,28 @@ public class SqlHelper {
         this.connectionFactory = connectionFactory;
     }
 
-    public <T> T executeAndProcessQuery(String query, SqlProcessor<T> sqlProcessor) {
+    public <T> T execute(String query, SqlProcessor<T> sqlProcessor) {
         try (Connection conn = connectionFactory.getConnection();
              PreparedStatement ps = conn.prepareStatement(query)) {
             return sqlProcessor.process(ps);
         } catch (SQLException e) {
             throw ExceptionUtil.convertException(e);
+        }
+    }
+
+    public <T> T transactionExecute(SqlTransaction<T> executor) {
+        try (Connection conn = connectionFactory.getConnection()) {
+            try {
+                conn.setAutoCommit(false);
+                T rs = executor.execute(conn);
+                conn.commit();
+                return rs;
+            } catch (SQLException e) {
+                conn.rollback();
+                throw ExceptionUtil.convertException(e);
+            }
+        } catch (SQLException e) {
+            throw new StorageException(e);
         }
     }
 }
