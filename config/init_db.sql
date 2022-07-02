0964f4a89fae6ac93d5deccc341eab722ac264b3
naliev <sms290@yandex.ru>
2022-07-02 21:44:37 +0300

    Add initialization of DB table Section to init file.

--- config/init_db.sql
+++ config/init_db.sql
@@ -15,3 +15,14 @@ create table contract
 );
 
 create unique index contract_resume_uuid_type on contract (resume_uuid, type);
+
+create table section
+(
+    id          serial constraint section_pk primary key,
+    type        text        not null,
+    value       text        not null,
+    resume_uuid char(36)    not null
+        constraint section_resume_uuid_fk references resume on update restrict on delete cascade
+);
+
+create unique index section_resume_uuid_type on section (resume_uuid, type);
