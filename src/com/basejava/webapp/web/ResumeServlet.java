a801e371312ec016a2802321fb824aff41369081
naliev <sms290@yandex.ru>
2022-07-04 23:16:23 +0300

    Init of JavaServerPage Resume in order to show resume table

--- src/com/basejava/webapp/web/ResumeServlet.java
+++ src/com/basejava/webapp/web/ResumeServlet.java
@@ -1,8 +1,6 @@
 package com.basejava.webapp.web;
 
 import com.basejava.webapp.Config;
-import com.basejava.webapp.model.ContactType;
-import com.basejava.webapp.model.Resume;
 import com.basejava.webapp.storage.SqlStorage;
 
 import javax.servlet.ServletConfig;
@@ -11,8 +9,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.Map;
 
 public class ResumeServlet extends HttpServlet {
 
@@ -26,29 +22,8 @@ public class ResumeServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        Map<String, String[]> parameterMap = request.getParameterMap();
-        response.setCharacterEncoding("UTF-8");
-        response.setContentType("text/html; charset=UTF-8");
-        PrintWriter writer = response.getWriter();
-        StringBuilder sB = new StringBuilder();
-        if (parameterMap.isEmpty()) {
-            sB.append("<table><tr><th> UUID </th><th> Name </th><th> Contacts </th></tr>");
-            for (Resume resume: storage.getAllSorted()) {
-                sB.append("<tr><td>").append(resume.getUuid()).append("</td>").
-                        append("<td>").append(resume.getFullName()).append("</td>").
-                        append("<td>").append(resume.getContacts().toString()).append("</td></tr>");
-            }
-            sB.append("</table>");
-        } else if (parameterMap.containsKey("uuid")) {
-            Resume r = storage.get(parameterMap.get("uuid")[0]);
-            sB.append(String.format("<h1> %S </h1> <br>", r.getFullName()));
-            for (Map.Entry<ContactType,String> entry: r.getContacts().entrySet()) {
-                sB.append(String.format("<b>%S:</b> %S <br>", entry.getKey().getTitle(), entry.getValue()));
-            }
-        } else {
-            sB.append("Error: unknown parameters sent");
-        }
-        writer.write(sB.toString());
+        request.setAttribute("resumes", storage.getAllSorted());
+        request.getRequestDispatcher("/WEB-INF/jsp/resume.jsp").forward(request,response);
     }
 
     @Override
---
+++ web/WEB-INF/jsp/resume.jsp
@@ -0,0 +1,23 @@
+<%@ page import="com.basejava.webapp.model.Resume" %>
+<%@ page import="java.util.List" %>
+<%@ page import="com.basejava.webapp.model.ContactType" %>
+<%@ page contentType="text/html;charset=UTF-8" %>
+<html>
+<head>
+    <title>Список резюме</title>
+</head>
+<body>
+<table>
+    <tr>
+        <th> Name</th>
+        <th> Email</th>
+    </tr>
+    <% for (Resume resume : (List<Resume>) request.getAttribute("resumes")) {%>
+    <tr>
+        <td><a href="resume?uuid = <%=resume.getUuid()%>"><%=resume.getFullName()%></a></td>
+        <td><%=resume.getContact(ContactType.EMAIL)%></td>
+    </tr>
+    <%}%>
+</table>
+</body>
+</html>
