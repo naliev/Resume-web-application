8fe60b1afeea9a764605113cef1909a4f3a7f175
naliev <sms290@yandex.ru>
2022-07-09 18:08:21 +0300

    Backup sections to test data

--- src/com/basejava/webapp/ResumeTestData.java
+++ src/com/basejava/webapp/ResumeTestData.java
@@ -1,9 +1,12 @@
 package com.basejava.webapp;
 
 import com.basejava.webapp.model.*;
+import com.basejava.webapp.util.DateUtil;
 
+import java.time.Month;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 
 public class ResumeTestData {
     public static void main(String[] args) {
@@ -42,15 +45,15 @@ public class ResumeTestData {
                 "logging", "Template method", "Singleton method"};
         resume.addSection(SectionType.QUALIFICATIONS, new ListSection(new ArrayList<>(Arrays.asList(qualificationList))));
 
-//        Organization vogu = new Organization("Vologda university", "vogu35", null);
-//        vogu.addPeriod(new Period(DateUtil.of(2017, Month.JULY), DateUtil.of(2017, Month.SEPTEMBER),
-//                "Preliminary Courses", null));
-//        vogu.addPeriod(new Period(DateUtil.of(2017, Month.SEPTEMBER), DateUtil.of(2021, Month.JUNE),
-//                "Undergraduate","Informatics and Computer Engineering"));
-//        Organization javaOps = new Organization("JavaOps", "JavaOps", "https://javaops.ru/");
-//        javaOps.addPeriod(new Period(DateUtil.of(2022, Month.MAY), DateUtil.NOW,
-//                "BaseJava", "Create a resume repository project"));
-//        resume.addSection(SectionType.EDUCATION, new OrganizationSection(new ArrayList<>(Arrays.asList(vogu, javaOps))));
+        Organization vogu = new Organization("Vologda university", "vogu35", null);
+        vogu.addPeriod(new Period(DateUtil.of(2017, Month.JULY), DateUtil.of(2017, Month.SEPTEMBER),
+                "Preliminary Courses", null));
+        vogu.addPeriod(new Period(DateUtil.of(2017, Month.SEPTEMBER), DateUtil.of(2021, Month.JUNE),
+                "Undergraduate","Informatics and Computer Engineering"));
+        Organization javaOps = new Organization("JavaOps", "JavaOps", "https://javaops.ru/");
+        javaOps.addPeriod(new Period(DateUtil.of(2022, Month.MAY), DateUtil.NOW,
+                "BaseJava", "Create a resume repository project"));
+        resume.addSection(SectionType.EDUCATION, new OrganizationSection(new ArrayList<>(Arrays.asList(vogu, javaOps))));
 
         return resume;
     }
@@ -63,17 +66,17 @@ public class ResumeTestData {
         String[] qualificationList = {"Java core", "Unit testing", "Spring", "Hibernate"};
         resume.addSection(SectionType.QUALIFICATIONS, new ListSection(new ArrayList<>(Arrays.asList(qualificationList))));
 
-//        Organization mgu = new Organization("Moscow government university", "mgu website", "msu.ru");
-//        mgu.addPeriod(new Period(DateUtil.of(2011, Month.SEPTEMBER), DateUtil.of(2015, Month.MAY),
-//                "Undergraduate", "Creation and automation of computing systems"));
-//        mgu.addPeriod(new Period(DateUtil.of(2015, Month.SEPTEMBER), DateUtil.of(2021, Month.JUNE),
-//                "Graduate school", "Creation and automation of computing systems"));
-//        resume.addSection(SectionType.EDUCATION, new OrganizationSection(new ArrayList<>(Collections.singletonList(mgu))));
-//        Organization epam = new Organization("EPAM Systems","Enterprice software development", "epam.com");
-//        epam.addPeriod(new Period(DateUtil.of(2015, Month.DECEMBER), DateUtil.NOW,
-//                "developer","Network related applications developer")
-//        );
-//        resume.addSection(SectionType.EXPERIENCE, new OrganizationSection(new ArrayList<>(Collections.singletonList(epam))));
+        Organization mgu = new Organization("Moscow government university", "mgu website", "msu.ru");
+        mgu.addPeriod(new Period(DateUtil.of(2011, Month.SEPTEMBER), DateUtil.of(2015, Month.MAY),
+                "Undergraduate", "Creation and automation of computing systems"));
+        mgu.addPeriod(new Period(DateUtil.of(2015, Month.SEPTEMBER), DateUtil.of(2021, Month.JUNE),
+                "Graduate school", "Creation and automation of computing systems"));
+        resume.addSection(SectionType.EDUCATION, new OrganizationSection(new ArrayList<>(Collections.singletonList(mgu))));
+        Organization epam = new Organization("EPAM Systems","Enterprice software development", "epam.com");
+        epam.addPeriod(new Period(DateUtil.of(2015, Month.DECEMBER), DateUtil.NOW,
+                "developer","Network related applications developer")
+        );
+        resume.addSection(SectionType.EXPERIENCE, new OrganizationSection(new ArrayList<>(Collections.singletonList(epam))));
         return resume;
     }
 
@@ -85,13 +88,13 @@ public class ResumeTestData {
         String[] qualificationList = {"Python core", "Django", "Laravel", "Kafka"};
         resume.addSection(SectionType.QUALIFICATIONS, new ListSection(new ArrayList<>(Arrays.asList(qualificationList))));
 
-//        Organization vogu = new Organization("Saints petersburg university", "spu", null);
-//        vogu.addPeriod(new Period(DateUtil.of(2015, Month.SEPTEMBER), DateUtil.of(2020, Month.JUNE),
-//                "Undergraduate","Informatics and Computer Engineering"));
-//        Organization javaOps = new Organization("JavaOps", "JavaOps", "https://javaops.ru/");
-//        javaOps.addPeriod(new Period(DateUtil.of(2018, Month.MAY), DateUtil.of(2018, Month.AUGUST),
-//                "TopJava", "Create a resume repository project"));
-//        resume.addSection(SectionType.EDUCATION, new OrganizationSection(new ArrayList<>(Arrays.asList(vogu, javaOps))));
+        Organization vogu = new Organization("Saints petersburg university", "spu", null);
+        vogu.addPeriod(new Period(DateUtil.of(2015, Month.SEPTEMBER), DateUtil.of(2020, Month.JUNE),
+                "Undergraduate","Informatics and Computer Engineering"));
+        Organization javaOps = new Organization("JavaOps", "JavaOps", "https://javaops.ru/");
+        javaOps.addPeriod(new Period(DateUtil.of(2018, Month.MAY), DateUtil.of(2018, Month.AUGUST),
+                "TopJava", "Create a resume repository project"));
+        resume.addSection(SectionType.EDUCATION, new OrganizationSection(new ArrayList<>(Arrays.asList(vogu, javaOps))));
 
         return resume;
     }
