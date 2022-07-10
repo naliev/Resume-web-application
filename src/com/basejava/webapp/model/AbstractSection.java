8008f1ef95a0e91b4c8587cc65c0ca84bc8a9821
naliev <sms290@yandex.ru>
2022-07-10 16:54:15 +0300

    Add Serialization in order to work with TomCat

--- src/com/basejava/webapp/model/AbstractSection.java
+++ src/com/basejava/webapp/model/AbstractSection.java
@@ -5,8 +5,9 @@ import com.google.gson.annotations.JsonAdapter;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
+import java.io.Serializable;
 
 @XmlAccessorType(XmlAccessType.FIELD)
 @JsonAdapter(JsonSectionAdapter.class)
-public abstract class AbstractSection {
+public abstract class AbstractSection implements Serializable {
 }
