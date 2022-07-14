e83becc74cd80b461a230c8aac77073d9c616d16
naliev <sms290@yandex.ru>
2022-07-14 23:43:42 +0300

    Rework config in order to work with Heroku

--- src/com/basejava/webapp/Config.java
+++ src/com/basejava/webapp/Config.java
@@ -5,23 +5,22 @@ import com.basejava.webapp.storage.SqlStorage;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.file.Files;
 import java.util.Properties;
 
 public class Config {
-    private static final File PROPS = new File(System.getProperty("homeDir"),"config/resumes.properties");
+    private static final String PROPS = "/resumes.properties";
     private static final Config INSTANCE = new Config();
     private final File storageDir;
     private final SqlStorage sqlStorage;
 
     private Config() {
-        try (InputStream is = Files.newInputStream(PROPS.toPath())) {
+        try (InputStream is = Config.class.getResourceAsStream(PROPS)) {
             Properties props = new Properties();
             props.load(is);
             storageDir = new File(props.getProperty("storage.dir"));
             sqlStorage = new SqlStorage(props.getProperty("db.url"), props.getProperty("db.user"), props.getProperty("db.password"));
         } catch (IOException e) {
-            throw new IllegalStateException("Invalid config file" + PROPS.getAbsolutePath());
+            throw new IllegalStateException("Invalid config file" + PROPS);
         }
     }
 
