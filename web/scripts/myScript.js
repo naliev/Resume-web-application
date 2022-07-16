0d6533c53766fbcdc02c64caa4d48ed3f770beaa
naliev <sms290@yandex.ru>
2022-07-16 23:37:56 +0300

    Added transition java script

---
+++ web/scripts/myScript.js
@@ -0,0 +1,11 @@
+$(document).on("scroll", function() {
+  if ($(document).scrollTop() > 10) {
+    $("header")
+      .removeClass("large")
+      .addClass("small");
+  } else {
+    $("header")
+      .removeClass("small")
+      .addClass("large");
+  }
+});
